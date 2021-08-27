83860359350cb55de6bfae99cb933908ae8078d7
Andrew Robinson <andrewmcrobinson@gmail.com>
2021-08-27 20:06:15 +0200

    required output with hardcoded postcode

--- main.go
+++ main.go
@@ -7,7 +7,19 @@ import (
 	"os"
 )
 
+// TODO
+// cat coordinates.txt | ./your-program "api token" "pool size flag" > output.txt
+
+func getPostcode(url string) string {
+	return "SE14 9AB"
+}
+
 func main() {
+
+	token := "pk.eyJ1IjoiYW5kcmV3bWNyb2JpbnNvbiIsImEiOiJja3N1bjlubG4wbnRrMnZsc3pwbnVscXJ1In0.9IqlyGEbz7lfcRGcHZdJPQ"
+
+	urlTemplate := "https://api.mapbox.com/geocoding/v5/mapbox.places/<%f,%f>.json?types=postcode&limit=1&access_token=%s"
+
 	file, err := os.Open("coordinates.txt")
 	if err != nil {
 		fmt.Println(err)
@@ -19,9 +31,11 @@ func main() {
 
 	lineNumber := 0
 
+	// { "lat": <float64>, "lng": <float64>, "postcode": <string> }
 	type coords struct {
-		Lat float32
-		Lng float32
+		Lat      float64 `json:"lat"`
+		Lng      float64 `json:"lng"`
+		Postcode string  `json:"postcode"`
 	}
 
 	for scanner.Scan() {
@@ -32,7 +46,16 @@ func main() {
 		coord := coords{}
 
 		json.Unmarshal([]byte(line), &coord)
-		fmt.Printf("coord:%+v\n", coord)
+
+		url := fmt.Sprintf(urlTemplate, coord.Lat, coord.Lng, token)
+
+		postcode := getPostcode(url)
+		coord.Postcode = postcode
+
+		// fmt.Printf("coord:%+v\n", coord)
+
+		coordStr, _ := json.Marshal(coord)
+		fmt.Println(string(coordStr))
 
 		// if lineNumber == 4 {
 		// 	break
