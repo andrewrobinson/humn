a75121499babdf3438a14e63f63225bdc2d907a3
Andrew Robinson <andrewmcrobinson@gmail.com>
2021-08-27 20:31:49 +0200

    more

--- main.go
+++ main.go
@@ -4,21 +4,22 @@ import (
 	"bufio"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"os"
 )
 
 // TODO
 // cat coordinates.txt | ./your-program "api token" "pool size flag" > output.txt
 
-func getPostcode(url string) string {
-	return "SE14 9AB"
+type Coord struct {
+	Lat      float64 `json:"lat"`
+	Lng      float64 `json:"lng"`
+	Postcode string  `json:"postcode"`
 }
 
 func main() {
-
-	token := "pk.eyJ1IjoiYW5kcmV3bWNyb2JpbnNvbiIsImEiOiJja3N1bjlubG4wbnRrMnZsc3pwbnVscXJ1In0.9IqlyGEbz7lfcRGcHZdJPQ"
-
-	urlTemplate := "https://api.mapbox.com/geocoding/v5/mapbox.places/<%f,%f>.json?types=postcode&limit=1&access_token=%s"
 
 	file, err := os.Open("coordinates.txt")
 	if err != nil {
@@ -32,23 +33,16 @@ func main() {
 	lineNumber := 0
 
 	// { "lat": <float64>, "lng": <float64>, "postcode": <string> }
-	type coords struct {
-		Lat      float64 `json:"lat"`
-		Lng      float64 `json:"lng"`
-		Postcode string  `json:"postcode"`
-	}
 
 	for scanner.Scan() {
 
 		lineNumber++
 		line := scanner.Text()
 
-		coord := coords{}
-
+		coord := Coord{}
 		json.Unmarshal([]byte(line), &coord)
 
-		url := fmt.Sprintf(urlTemplate, coord.Lat, coord.Lng, token)
-
+		url := getUrl(coord)
 		postcode := getPostcode(url)
 		coord.Postcode = postcode
 
@@ -67,4 +61,34 @@ func main() {
 		fmt.Printf("error on line %v: %v", lineNumber, err)
 	}
 
+}
+
+func getUrl(coord Coord) string {
+
+	token := "pk.eyJ1IjoiYW5kcmV3bWNyb2JpbnNvbiIsImEiOiJja3N1bjlubG4wbnRrMnZsc3pwbnVscXJ1In0.9IqlyGEbz7lfcRGcHZdJPQ"
+
+	// https://docs.mapbox.com/api/search/#geocoding
+
+	urlTemplate := "https://api.mapbox.com/geocoding/v5/mapbox.places/<%f,%f>.json?types=postcode&limit=1&access_token=%s"
+	return fmt.Sprintf(urlTemplate, coord.Lat, coord.Lng, token)
+
+}
+
+func getPostcode(url string) string {
+	// fmt.Printf("getPostcode with url:%s", url)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	//We Read the response body on the line below.
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	//Convert the body to type string
+	sb := string(body)
+	log.Printf(sb)
+
+	return "SE14 9AB"
 }
