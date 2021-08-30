584d592990ccad7f949d11a301199b1ddbd3da91
Andrew Robinson <andrewmcrobinson@gmail.com>
2021-08-30 08:24:23 +0200

    flag stuff

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io"
 	"log"
@@ -11,30 +12,28 @@ import (
 	"github.com/andrewrobinson/humn/model"
 )
 
-// cat coordinates.txt | ./humn "api token" "pool size flag" > output.txt
-
 func main() {
 
 	/*
 
-		Started 19h40
-		21h04 - Now I started finding code on the internet in order to achieve:
+		Usage:
 
-		- Stdin should be read by a separate single goroutine and not directly by the worker pool.
-		- Stdout should be written to from a separate single goroutine and not directly from the worker pool.
-
-		//1st I wrote what you see in main2
-
-		//Then I used this as a base. It doesn't actually use channels yet....
-		// https://play.golang.org/p/OSS71nSpkV
-
-		22h10 - am wondering if chanOwnerChanConsumer or produceReceive() can help me
-
-		22h20 - back to reading https://gobyexample.com/channels
-
-
+		go build
+		cat coordinates.txt | ./humn --apiToken=asd --poolSize=4 > output.txt
 
 	*/
+
+	apiTokenFlag := flag.String("apiToken", "", "no default")
+	poolSizeFlag := flag.Int("poolSize", 5, "The number of goroutine for the worker pool")
+
+	flag.Parse()
+
+	if flag.Lookup("apiToken").Value.String() == "" {
+		fmt.Println("apiToken NOT present")
+		os.Exit(1)
+	}
+
+	fmt.Printf("apiTokenFlag:%s, poolSizeFlag:%d\n\n", *apiTokenFlag, *poolSizeFlag)
 
 	rdr := bufio.NewReader(os.Stdin)
 	out := os.Stdout
@@ -75,53 +74,53 @@ func main() {
 	}
 }
 
-func produceReceive() {
-	//from Concurrency in Go
-	//ch4
+// func produceReceive() {
+// 	//from Concurrency in Go
+// 	//ch4
 
-	chanOwner := func() <-chan int {
-		resultStream := make(chan int, 5)
-		go func() {
-			defer close(resultStream)
-			for i := 0; i <= 5; i++ {
-				resultStream <- i
-			}
-		}()
-		return resultStream
-	}
+// 	chanOwner := func() <-chan int {
+// 		resultStream := make(chan int, 5)
+// 		go func() {
+// 			defer close(resultStream)
+// 			for i := 0; i <= 5; i++ {
+// 				resultStream <- i
+// 			}
+// 		}()
+// 		return resultStream
+// 	}
 
-	resultStream := chanOwner()
-	for result := range resultStream {
-		fmt.Printf("Received: %d\n", result)
-	}
-	fmt.Println("Done receiving!")
+// 	resultStream := chanOwner()
+// 	for result := range resultStream {
+// 		fmt.Printf("Received: %d\n", result)
+// 	}
+// 	fmt.Println("Done receiving!")
 
-}
+// }
 
-func chanOwnerChanConsumer() {
-	//from Concurrency in Go
-	//ch4 - confinement - 1
-	//this format keeps the responsibilities of the 2 roles
+// func chanOwnerChanConsumer() {
+// 	//from Concurrency in Go
+// 	//ch4 - confinement - 1
+// 	//this format keeps the responsibilities of the 2 roles
 
-	chanOwner := func() <-chan int {
-		results := make(chan int, 5)
-		go func() {
-			defer close(results)
-			for i := 0; i <= 5; i++ {
-				results <- i
-			}
-		}()
-		return results
-	}
+// 	chanOwner := func() <-chan int {
+// 		results := make(chan int, 5)
+// 		go func() {
+// 			defer close(results)
+// 			for i := 0; i <= 5; i++ {
+// 				results <- i
+// 			}
+// 		}()
+// 		return results
+// 	}
 
-	consumer := func(results <-chan int) {
-		for result := range results {
-			fmt.Printf("Received: %d\n", result)
-		}
-		fmt.Println("Done receiving!")
-	}
+// 	consumer := func(results <-chan int) {
+// 		for result := range results {
+// 			fmt.Printf("Received: %d\n", result)
+// 		}
+// 		fmt.Println("Done receiving!")
+// 	}
 
-	results := chanOwner()
-	consumer(results)
+// 	results := chanOwner()
+// 	consumer(results)
 
-}
+// }
